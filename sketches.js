2d6dffbb73b2f6a282ad6a9d88f1507ce2690e4c
Colin Drake <github@accounts.colindrake.me>
2020-05-11 00:07:37 -0500

    Add modulated noise

--- sketches.js
+++ sketches.js
@@ -14,3 +14,17 @@ osc(100, 0.1, 1)
   .modulate(noise(2).add(osc(32))).diff(shape(3).modulate(noise(0.8)).modulateScale(osc(2)))
   .modulate(o0, 0.01)
   .out()
+
+// posterized red/blue gummy scroller
+osc(20)
+  .modulateScale(osc(10), 100)
+  .pixelate()
+  .color(0, 1, 1)
+  .mult(osc(20).modulate(noise())).add(osc().scale(2).color(1, 0, 1), 0.5)
+  .hue(0.1)
+  .posterize()
+  .scale(2)
+  .out()
+
+// modulated noise pattern
+noise(100).modulate(osc().modulateScale(osc(8))).modulateScrollY(osc(4)).out()
